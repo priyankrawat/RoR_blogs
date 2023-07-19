b9d12a2386e1381389139b9d39b1c6e3c779b48e
Priyank Rawat <ultimatebeast.7@gmail.com>
2023-07-19 17:48:17 +0530

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   resources :posts
   get 'about', to: 'pages#about'
 
+
+  resources :posts do
+    resources :comments
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/migrate/20230718190057_change_views_for_users.rb
+++ db/migrate/20230718190057_change_views_for_users.rb
@@ -1,4 +1,5 @@
 class ChangeViewsForUsers < ActiveRecord::Migration[7.0]
   def change
+    
   end
 end
