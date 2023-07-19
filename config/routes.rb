279f8e157a68959bdba06100c6dbcaa57db29a77
Priyank Rawat <ultimatebeast.7@gmail.com>
2023-07-19 16:06:58 +0530

    New migrations added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/profile'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   resources :posts
   get 'about', to: 'pages#about'
 
