a5a21c18ee2a8fd3026c95bc1c0dd43083c0e34b
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2024-01-17 14:12:47 +0530

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_19_101324) do  
-  create_table "action_text_rich_texts", force: :cascade do |t|  
+  create_table "action_text_rich_texts", force: :cascade do |t|   
     t.string "name", null: false
     t.text "body"
     t.string "record_type", null: false 
