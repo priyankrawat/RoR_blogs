7ae0d0221eeb489629b52e5cbbdb527509ed77dd
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2024-01-17 14:12:58 +0530

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_19_101324) do
   create_table "action_text_rich_texts", force: :cascade do |t|   
     t.string "name", null: false 
     t.text "body"
-    t.string "record_type", null: false 
+    t.string "record_type", null: false  
     t.bigint "record_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
